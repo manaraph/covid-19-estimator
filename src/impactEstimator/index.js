7720a60f1ab88add628291553be8758a39151425
manaraph <manaraph225@gmail.com>
2020-04-10 15:00:57 +0100

    Estimate infections by requested time

--- src/impactEstimator/index.js
+++ src/impactEstimator/index.js
@@ -1,9 +1,11 @@
 import {
-  computeCurrentlyInfected
+  computeCurrentlyInfected,
+  computeInfectionsByRequestedTime
 } from './util';
 
 const estimateImpact = (data, impactFactor) => ({
   currentlyInfected: computeCurrentlyInfected(data, impactFactor),
+  infectionsByRequestedTime: computeInfectionsByRequestedTime(data , impactFactor),
 });
 
 export default estimateImpact;
--- src/impactEstimator/util.js
+++ src/impactEstimator/util.js
@@ -1,7 +1,29 @@
+const computeInfectionRate = ({ periodType, timeToElapse }) => {
+  let duration = 0;
+
+  switch (periodType) {
+    case 'weeks' || 'week':
+      duration = timeToElapse * 7;
+    case 'months' || 'month':
+      duration = timeToElapse * 30;
+    case 'years' || 'year':
+      duration = timeToElapse * 360;
+    default:
+      duration = timeToElapse;
+  };
+
+  const infectionRate = 2 * Math.trunc(duration/3);
+  return infectionRate;
+};
 
 const computeCurrentlyInfected = ({ reportedCases }, impactFactor) => reportedCases * impactFactor;
+const computeInfectionsByRequestedTime = (data , impactFactor) => {
+  const currentlyInfected = computeCurrentlyInfected(data, impactFactor);
+  const infectionRate = computeInfectionRate(data);
+  return currentlyInfected * infectionRate;
+};
 
 module.exports = {
   computeCurrentlyInfected,
-
+  computeInfectionsByRequestedTime
 }
