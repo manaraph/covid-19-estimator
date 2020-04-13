1d7c368234bddd063f0b3f86e7e7e012047ba161
manaraph <manaraph225@gmail.com>
2020-04-13 12:57:47 +0100

    return estimates when covid data is posted

--- api/routes/estimates.js
+++ api/routes/estimates.js
@@ -3,20 +3,14 @@ import { jsonEstimates } from "../controllers/estimatesControllers";
 const router = express.Router();
 
 router.post('/on-covid-19', (req, res) => {
-  console.log(req);
-  
-  // const { body } = req;
-  // console.log(body);
-  
-  // try {
-  //   const estimates = jsonEstimates(body);
-  //   console.log(estimates);
-  // } catch (error) {
-  //   console.log(error);
-    
-  // }
-  res.status(200).send('Hello World!');
-
+  const { body } = req;  
+  try {
+    const estimates = jsonEstimates(body);
+    res.status(200).send(estimates);
+  } catch (error) {
+    // console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 router.get('/on-covid-19/logs', (req, res) => {
