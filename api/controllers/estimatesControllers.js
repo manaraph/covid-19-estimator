f3198b2e40fb6e308f505e05edf4d16d328b09d6
manaraph <manaraph225@gmail.com>
2020-04-13 14:00:03 +0100

    Progress with return json and xml response

--- api/controllers/estimatesControllers.js
+++ api/controllers/estimatesControllers.js
@@ -1,10 +1,24 @@
 import covid19ImpactEstimator from "../../src/estimator";
+import { toXML } from 'jstoxml';
+
+const xmlOptions = {
+  header: false,
+  indent: '  '
+};
 
 const jsonEstimates = (data) => {
   return covid19ImpactEstimator(data);
 };
 
+const xmlEstimates = (data) => {
+  const jsonEstimates = jsonEstimates(data);
+  const estimates = toXML(jsonEstimates, xmlOptions);
+  console.log(estimates);
+  return estimates;
+};
+
 module.exports = {
   jsonEstimates,
+  xmlEstimates
 }
 
--- api/routes/estimates.js
+++ api/routes/estimates.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { jsonEstimates } from "../controllers/estimatesControllers";
+import { jsonEstimates, xmlEstimates } from "../controllers/estimatesControllers";
 const router = express.Router();
 
 router.post('/on-covid-19', (req, res) => {
@@ -8,8 +8,28 @@ router.post('/on-covid-19', (req, res) => {
     const estimates = jsonEstimates(body);
     res.status(200).send(estimates);
   } catch (error) {
-    // console.log(error);
     res.status(500).send(error);
+  }
+});
+
+router.post('/on-covid-19/json', (req, res) => {
+  const { body } = req;  
+  try {
+    const estimates = jsonEstimates(body);
+    res.status(200).send(estimates);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/on-covid-19/xml', (req, res) => {
+  const { body } = req;  
+  try {
+    const estimates = xmlEstimates(body);
+    console.log(estimates);
+    res.status(200).type('application/xml').send(estimates);
+  } catch (error) {
+    res.status(500).type('application/xml').send(error);
   }
 });
 
